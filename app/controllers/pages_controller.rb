f76d362f5a7fbd6eb4296967b23af7bd1329a6cb
Kutomore <kutomore@gmail.com>
2020-04-17 23:14:48 -0300

    Adding static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  def home; end
+
+  def about; end
+
+  def contact; end
+
+  def help; end
+
+  def terms_of_service; end
+
+  def landing_page; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   resources :contact_infos
   resources :people
 
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/help', to: 'pages#help'
+  get '/terms-of-service', to: 'pages#terms_of_service'
+  get '/landing-page', to: 'pages#landing_page'
+
   devise_for :users
-  root to: 'people#index'
+  authenticated do
+    root to: 'pages#home', as: :authenticated_root
+  end
+
+  root to: 'pages#landing_page'
 end
