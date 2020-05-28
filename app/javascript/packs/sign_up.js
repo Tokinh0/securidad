63dfffda88c7c05a5e58ea5147e813fd65b32d8e
Kutomore <kutomore@gmail.com>
2020-05-28 12:08:46 -0300

    Fixing

--- app/javascript/packs/sign_up.js
+++ app/javascript/packs/sign_up.js
@@ -1,31 +1,38 @@
 let currentStep = 1
 const progressBar = document.querySelector('.progress-bar')
 
-document.addEventListener("turbolinks:load", function() {
-    document.getElementById('next-login-step').addEventListener('click', function () {
-        switch (currentStep) {
-            case 1:
-                document.querySelectorAll('.first-step').forEach(function (element) {
-                    element.classList.add('hidden')
-                })
-                document.querySelectorAll('.second-step').forEach(function (element) {
-                    element.classList.remove('hidden')
-                })
-                currentStep = 2
-                progressBar.style.width = '60%'
-                break;
-            case 2:
-                document.querySelectorAll('.second-step').forEach(function (element) {
-                    element.classList.add('hidden')
-                })
-                document.querySelectorAll('.third-step').forEach(function (element) {
-                    element.classList.remove('hidden')
-                })
-                currentStep = 3
-                progressBar.style.width = '90%'
-                break;
-            default:
-                break;
-        }
-    })
+document.getElementById('new_user').addEventListener('submit', function (event) {
+    if(currentStep !== 3){
+        event.preventDefault();
+        advanceStep();
+    }
 })
+
+document.getElementById('next-login-step').addEventListener('click', advanceStep)
+
+function advanceStep() {
+    switch (currentStep) {
+        case 1:
+            document.querySelectorAll('.first-step').forEach(function (element) {
+                element.classList.add('hidden')
+            })
+            document.querySelectorAll('.second-step').forEach(function (element) {
+                element.classList.remove('hidden')
+            })
+            currentStep = 2
+            progressBar.style.width = '60%'
+            break;
+        case 2:
+            document.querySelectorAll('.second-step').forEach(function (element) {
+                element.classList.add('hidden')
+            })
+            document.querySelectorAll('.third-step').forEach(function (element) {
+                element.classList.remove('hidden')
+            })
+            currentStep = 3
+            progressBar.style.width = '90%'
+            break;
+        default:
+            break;
+    }
+}
